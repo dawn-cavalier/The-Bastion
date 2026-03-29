6728150a91528a1925d6f6420ab335a41f2fd52c
Dawn Cavalier <ashley.r.toper@gmail.com>
2026-03-29 14:07:46 -0400

    initial role grid

--- game/python-demo/brain.py
+++ game/python-demo/brain.py
@@ -1,38 +1,127 @@
-import random as r  
+import random as r
 
 from enums.roles import *
+from brainHelper import *
+from enums.infoType import *
+
+
+class Knowledge:
+    day: int
+    source: int | None
+    target: int | None
+    infoType: InfoType
+    information: any
+
+    def __init__(
+        self,
+        day: int,
+        source: int | None,
+        target: int | None,
+        infoType: InfoType,
+        information: any,
+    ):
+        self.day = day
+        self.source = source
+        self.target = target
+        self.infoType = infoType
+        self.information = information
+
+    def __str__(self) -> str:
+        if self.infoType is InfoType.IS_ROLE:
+            if self.source is None:
+                return f"On day {self.day}, Seat {self.target} learned {self.infoType.name} {self.information.name} from the Storyteller."
+            return f"On day {self.day}, Seat {self.target} learned {self.infoType.name} {self.information.name} from Seat {self.source}."
+
+        return "UNHANDLED"
+
 
 class Player:
     role: Role
-    seat: int 
-    isDrunk: bool
-    def __init__(self, seat, role) -> None:
+    seat: int
+    reminderTokens: list[Status]
+    knowledgeBank: list[Knowledge]
+    socialTrust: list[float]
+    roleGrid: list[list[float]]
+
+    def __init__(self, seat: int, role: Role, isDrunk: bool, playerCount: int) -> None:
         self.seat = seat
         self.role = role
-        self.isDrunk = False
+        self.reminderTokens = []
+        self.knowledgeBank = []
+        if isDrunk:
+            self.reminderTokens.append(Status.IS_DRUNK)
+
+        self.socialTrust = [0.5 for _ in range(playerCount)]
+        self.roleGrid = [
+            [0.0 for role in Role if role >= Role.WASHERWOMAN]
+            for seat in range(playerCount)
+        ]
+
+        presentTownsfolk, presentOutsider, presentMinion, presentDemon = getTypeCounts(
+            playerCount
+        )
+
+        townsfolkCount = len([_ for _ in Role if isTownsfolk(_)])
+        outsiderCount = len([_ for _ in Role if isOutsider(_)])
+        minionCount = len([_ for _ in Role if isMinion(_)])
+        demonCount = len([_ for _ in Role if isDemon(_)])
+
+        for playerSeat in range(playerCount):
+            for playerRole in [_ for _ in Role if _ >= Role.WASHERWOMAN]:
+                roleChance = 0.0
+                if isTownsfolk(playerRole):
+                    roleChance = presentTownsfolk / townsfolkCount
+
+                elif isOutsider(playerRole):
+                    roleChance = presentOutsider / outsiderCount
+
+                elif isMinion(playerRole):
+                    roleChance = presentMinion / minionCount
+
+                elif isDemon(playerRole):
+                    roleChance = presentDemon / demonCount
+
+                else:
+                    raise Exception("Invalid Role")
+
+                roleChance = roleChance / playerCount
+                self.roleGrid[playerSeat][playerRole] = roleChance
 
     def __str__(self) -> str:
-        return f"Seat: {self.seat}\nRole: {self.role.name}\nIs Drunk: {self.isDrunk}"
-    
-    def setDrunk(self, state) -> None:
-        self.isDrunk = state
+        return f"Seat: {self.seat}\nRole: {self.role.name}\n"
 
 
-def isTownsfolk(role: Role) -> bool:
-    return role >= Role.WASHERWOMAN and role <= Role.MAYOR
-def isOutsider(role: Role) -> bool:
-    return role >= Role.BUTLER and role <= Role.DRUNK
-def isMinion(role: Role) -> bool:
-    return role >= Role.POISONER and role <= Role.SCARLET_WOMAN
-def isDemon(role: Role) -> bool:
-    return role >= Role.IMP and role <= Role.IMP
+def main() -> None:
+    r.seed(a=0, version=2)
+    playerCount = 8
+    roles, drunkRole = getRoles(playerCount)
+    players = [
+        Player(
+            seat=seat,
+            role=roles[seat],
+            isDrunk=roles[seat] is drunkRole,
+            playerCount=playerCount,
+        )
+        for seat in list(range(playerCount))
+    ]
 
-def getRoles() -> tuple[list, Role]:
-    # Fill Bag
-    townsfolkCount = 7
-    outsiderCount = 2
-    minionCount = 2
-    demonCount = 1
+    learnStartingInfo(players)
+
+
+def learnStartingInfo(players: list[Player]) -> None:
+    for player in players:
+        learnedInfo = Knowledge(
+            day=0,
+            source=None,
+            target=player.seat,
+            infoType=InfoType.IS_ROLE,
+            information=player.role,
+        )
+        # print(learnedInfo)
+
+
+def getRoles(playerCount: int) -> tuple[list, Role]:
+    townsfolkCount, outsiderCount, minionCount, demonCount = getTypeCounts(playerCount)
 
     activeRoles: list[Role] = []
     allRoles = [role for role in Role if role > -1]
@@ -44,14 +133,18 @@ def getRoles() -> tuple[list, Role]:
         townsfolkCount -= 2
         outsiderCount += 2
 
-    activeRoles += r.sample([role for role in allRoles if isOutsider(role)], outsiderCount)
+    activeRoles += r.sample(
+        [role for role in allRoles if isOutsider(role)], outsiderCount
+    )
     if Role.DRUNK in activeRoles:
         townsfolkCount += 1
         outsiderCount -= 1
         activeRoles.remove(Role.DRUNK)
         drunkPresent = True
 
-    activeRoles += r.sample([role for role in allRoles if isTownsfolk(role)], townsfolkCount)
+    activeRoles += r.sample(
+        [role for role in allRoles if isTownsfolk(role)], townsfolkCount
+    )
     if drunkPresent:
         drunkRole = r.sample([role for role in activeRoles if isTownsfolk(role)], 1)[0]
 
@@ -60,19 +153,6 @@ def getRoles() -> tuple[list, Role]:
 
     return (activeRoles, drunkRole)
 
-def main() -> None:
-    r.seed(a=None, version=2)
-
-    roles, drunkRole = getRoles()
-    players = [Player(seat, roles[seat]) for seat in list(range(12))]
-    drunkPlayers = [player for player in players if player.role is drunkRole]
-    if len(drunkPlayers) > 0:
-        for player in drunkPlayers:
-            player.setDrunk(True)
-
-    for player in players:
-        print(player)
-    return
 
 if __name__ == "__main__":
     main()
---
+++ game/python-demo/enums/infoType.py
@@ -0,0 +1,4 @@
+from enum import IntEnum
+
+class InfoType(IntEnum):
+    IS_ROLE = 0
